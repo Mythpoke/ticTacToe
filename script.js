c9930a4200ccafa4d392ec5fbc96926373b36d4f
Maciek <maciek.jamroz@post.pl>
2024-09-13 16:53:25 +0200

    Add grid and dialog

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
-const gridContainer = document.getElementById("gridContainer");
+function makeGrid() {
+    const gridContainer = document.getElementById("gridContainer");
 
-for(let i = 0; i < 9; i++) {
-    let cube = document.createElement("div");
-    cube.classList.add("cube");
-    gridContainer.appendChild(cube);
+    for(let i = 0; i < 9; i++) {
+        let cube = document.createElement("button");
+        cube.classList.add("cube");
+        gridContainer.appendChild(cube);
 
-    cube.style.width = 400 / 3 + "px";
-    cube.style.height = 400 / 3 + "px";
-    cube.textContent = "X";
-}
+        cube.style.width = 400 / 3 + "px";
+        cube.style.height = 400 / 3 + "px";
+        cube.textContent = "X";
+    }
+} 
+
+makeGrid();
+
+const start = document.getElementById("start");
+const dialog = document.querySelector("dialog");
+const playerOne = document.getElementById("playerOne");
+const playerTwo = document.getElementById("playerTwo");
+const markOne = document.getElementById("markOne");
+const markTwo = document.getElementById("markTwo");
+const submit = document.getElementById("submit");
+
+const turn = document.getElementById("turn");
+
+start.addEventListener("click", ()=> {
+    dialog.showModal()
+;})
+submit.addEventListener("click", ()=> {
+    dialog.close();
+    console.log(submit.value);
+    turn.textContent = playerOne.value;
+;})
+
